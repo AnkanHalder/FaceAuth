eae62d0b22af06736936a32862b07d2265279022
AnkanHalder <halderankan95@gmail.com>
2023-10-14 14:38:48 +0530

    update gitignore

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import HomePage from '@/PageComponents/HomePage'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-8  md:p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center">
       <HomePage/>
     </main>
   )
